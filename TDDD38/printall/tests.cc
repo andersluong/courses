90a72b77c2b004250361f8906ca4f543e9183290
Anders Luong <andersluong@gmail.com>
2023-12-12 21:17:13 +0100

    printall exercise

--- TDDD38/printall/tests.cc
+++ TDDD38/printall/tests.cc
@@ -1,5 +1,4 @@
 #include <array>
-#include <cstddef>
 #include <iostream>
 #include <iterator>
 #include <map>
@@ -19,43 +18,75 @@
 SFINAE
 string literal
  */
-void print(std::ostream &os, char *const &n, int) { os << n; }
 
-void print(std::ostream &os, std::string const &n, long) { os << n; }
+template <typename T> void print(std::ostream &os, T const &value);
 
-template <typename T, std::size_t N>
-void print(std::ostream &os, std::array<T, N> const &container, int) {
-  os << '{';
-  for (auto it = std::begin(container); it != std::end(container); it++) {
-    // os << *it;
+namespace details {
+
+void print(std::ostream &os, std::string const &value, int) { os << value; }
+void print(std::ostream &os, char const *value, int) { os << value; }
+
+template <size_t N>
+void print(std::ostream &os, std::string const (&value)[N], int) {
+  os << "{";
+  for (auto it = std::begin(value); it != std::end(value) - 1; ++it) {
+    ::print(os, *it);
+    os << ", ";
   }
-  os << '}';
+  ::print(os, *(std::end(value) - 1));
+  os << "}";
 }
 
 template <typename T>
-auto print(std::ostream &os, T const &container, int)
-    -> decltype(std::begin(container), std::declval<void>()) {
-  os << '{';
-  for (auto it = std::begin(container); it != std::end(container); it++) {
-    // os << *it;
+auto print(std::ostream &os, T const &value, int)
+    -> decltype(std::begin(value), std::declval<void>()) {
+  os << "{";
+  for (auto it = std::begin(value); it != std::end(value); ++it) {
+    ::print(os, *it);
+    os << ", ";
   }
-  os << '}';
+  os << "}";
+}
+
+template <typename T> void print_with_space(std::ostream &os, T const &value) {
+  ::print(os, value);
+  os << " ";
+}
+
+template <typename T, std::size_t... Is>
+void print_tuple(std::ostream &os, T const &tuple, std::index_sequence<Is...>) {
+  (print_with_space(os, std::get<Is>(tuple)), ...);
 }
 
 template <typename T>
-auto print(std::ostream &os, T const &container, int)
-    -> decltype(std::get<0>(container), std::tuple_size_v<decltype(container)>,
+auto print(std::ostream &os, T const value, int)
+    -> decltype(std::get<0>(value), std::tuple_size_v<T>,
                 std::declval<void>()) {
-  os << '(';
-  os << ')';
+  constexpr size_t tuple_size = std::tuple_size_v<decltype(value)>;
+  os << "(";
+  print_tuple(os, value, std::make_index_sequence<tuple_size>{});
+  os << ")";
 }
 
-template <typename T> void print(std::ostream &os, T container, long) {
-  os << container;
+template <typename T, size_t N>
+void print(std::ostream &os, std::array<T, N> const &value, int) {
+  os << "{";
+  for (auto it = std::begin(value); it != std::end(value) - 1; ++it) {
+    ::print(os, *it);
+    os << ", ";
+  }
+  ::print(os, *(std::end(value) - 1));
+  os << "}";
 }
+
+template <typename T> void print(std::ostream &os, T const &value, double) {
+  os << value;
+}
+
+} // namespace details
 
 template <typename T> void print(std::ostream &os, T const &value) {
-  print(os, value, 0);
+  details::print(os, value, 0);
 }
 
 int main() {
