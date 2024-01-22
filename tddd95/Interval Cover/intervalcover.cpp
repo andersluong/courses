7bf8a207eb682c714dc6370662b63426f921412d
Anders Luong <andersluong@gmail.com>
2024-01-22 19:58:53 +0100

    interval done

--- tddd95/Interval Cover/intervalcover.cpp
+++ tddd95/Interval Cover/intervalcover.cpp
@@ -1,41 +1,47 @@
+/**
+* Using greedy algorithm to solve the interval cover problem.
+* The algorithm is as follow:
+* Sort the intervals on their starting point, then iterate through them until start isn't covered. Out of those
+* that are iterated, find the biggest covering area. Pick this one, and repeat until the end is covered.
+*
+* Source of inspiration: https://www.geeksforgeeks.org/minimum-number-of-intervals-to-cover-the-target-interval/
+* 
+* Author: Anders Luong (andlu434)
+*/
 #include <iostream>
 #include <vector>
 #include <limits>
-#include <string>
 #include <algorithm>
 
+// constants
 const static int INF = std::numeric_limits<int>::max();
 const static int END_OF_INPUT = -999;
 
+// Interval containing [lower, upper] and the index of the interval
 struct Interval {
   double lower, upper;
   int index;
 };
 
-Interval readInterval(int const i) {
-  std::string input;
-  if (!(std::cin >> input)) {
+// read an interval from the terminal
+Interval readInterval(int const index) {
+  double lower, upper;
+  if (!(std::cin >> lower >> upper)) {
     return Interval{ 0, 0, END_OF_INPUT  };
   }
-  double lower = std::stod(input);
-  if (!(std::cin >> input)) {
-    return Interval{ 0, 0, END_OF_INPUT };
-  }
-  double upper = std::stod(input);
-  return Interval{ lower, upper, i };
+  return Interval{ lower, upper, index };
 }
 
+// solving a test case. Here is where the algorithm is implemented
 bool solveTestcase() {
   Interval mainInterval{ readInterval(-1) };
   if (mainInterval.index == END_OF_INPUT) {
     return false;
   }
 
-  // source: https://www.geeksforgeeks.org/minimum-number-of-intervals-to-cover-the-target-interval/
   std::vector<Interval> intervals;
-  std::string input;
-  std::cin >> input;
-  int numberOfIntervals = std::stoi(input);
+  int numberOfIntervals;
+  std::cin >> numberOfIntervals;
   for (int i = 0; i < numberOfIntervals; i++) {
     intervals.push_back(readInterval(i));
   }
@@ -46,43 +52,46 @@ bool solveTestcase() {
   std::sort(intervals.begin(), intervals.end(), comparator);
 
   // init
-  std::string chosenIndexes;
-  double start = mainInterval.lower;
-  double end = mainInterval.lower - 1;
-  int count = 0;
+  std::vector<int> chosenIndexes;
   unsigned index = 0;
-
-  // special case when no intervals cover start
-  if (intervals[0].lower > start) {
-    std::cout << "impossible\n";
-    return true;
-  }
 
   // algorithm
   while (true) {
     // check if interval covers start
-    if (intervals[index].lower > start) {
+    if (intervals[index].lower > mainInterval.lower) {
       std::cout << "impossible\n";
       return true;
     } 
     // find biggest covering of start, since they are ordered of start, just iterate until the 
     // last one covering start
+    Interval biggestCovering{ mainInterval.lower, mainInterval.lower - 1, -1 };
     for (unsigned i = index; i < intervals.size(); i++) {
-      if (intervals[index].lower <= start) {
-        end = std::max(end, intervals[index].upper);
+      if (intervals[index].lower <= mainInterval.lower) {
+        if (intervals[index].upper > biggestCovering.upper) {
+          // found a better option
+          biggestCovering = intervals[index];
+        }
         index++;
+      } else {
+        break;
       }
     }
 
-    start = end;
+    // update state
+    mainInterval.lower = biggestCovering.upper;
+    chosenIndexes.push_back(biggestCovering.index);
+
+    // check if we are done
+    if (biggestCovering.upper >= mainInterval.upper) {
+      break;
+    }
   }
-  if (end < mainInterval.upper) {
-    std::cout << "impossible\n";
-    return true;
+  // found optimal solution, print it out
+  std::cout << chosenIndexes.size() << "\n";
+  for (auto index : chosenIndexes) {
+    std::cout << index << " ";
   }
-  chosenIndexes.pop_back();
-  std::cout << count << "\n";
-  std::cout << chosenIndexes << "\n";
+  std::cout << "\n";
 
   return true;
 }
