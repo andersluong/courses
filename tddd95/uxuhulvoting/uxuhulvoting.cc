36d4dca6274a29b9c997bf926921c35500b5ed6f
Anders Luong <andersluong@gmail.com>
2024-02-05 23:11:34 +0100

    no work

--- tddd95/uxuhulvoting/uxuhulvoting.cc
+++ tddd95/uxuhulvoting/uxuhulvoting.cc
@@ -1,50 +1,45 @@
 #include <iostream>
+#include <array>
 #include <bitset> 
-#include <vector>
 
-std::string orderToVoting(int const n) {
-  switch (n) {
-    case 1:
-      return "NNN";
-    case 2:
-      return "NNY";
-    case 3:
-      return "NYN";
-    case 4:
-      return "NYY";
-    case 5:
-      return "YNN";
-    case 6:
-      return "YNY";
-    case 7:
-      return "YYN";
-    case 8:
-      return "YYY";
-    default:
-      return "---";
+using stonesType = std::array<int, 8>;
+using worldMapType = std::array<stonesType, 101>;
+
+static const int UNDEFINED = -1;
+
+int flip(int const stone, int const index) {
+  return stone ^ (1 << index);
+}
+
+void checkStone(worldMapType &worldMap, worldMapType &preferences, int const priest, int const choice, int const stone) {
+  int nextStone = worldMap[priest + 1][choice];
+  if (nextStone == UNDEFINED) {
+    // this stone is not yet defined, so update it
+    worldMap[priest + 1][choice] = stone;
+  } else if (preferences[priest][choice] < preferences[priest][nextStone]) {
+    // this new priest prefers this stone more, so update it
+    worldMap[priest + 1][choice] = stone;
   }
 }
 
-std::string orderToBinary(int const n) {
-  switch (n) {
-    case 1:
-      return "000";
-    case 2:
-      return "001";
-    case 3:
-      return "010";
-    case 4:
-      return "011";
-    case 5:
-      return "100";
-    case 6:
-      return "101";
-    case 7:
-      return "110";
-    case 8:
-      return "111";
-    default:
-      return "---";
+void solve(worldMapType &worldMap, worldMapType &preferences, int const numberOfPriests) {
+  // when there are 0 priests, we start with NNN
+  worldMap[0][0] = 0b000;
+
+  for (int priest = 0; priest < numberOfPriests; priest++) {
+    stonesType stones = worldMap[priest];
+    for (unsigned stone = 0b000; stone < stones.size(); stone++) {
+      if (stones[stone] == UNDEFINED) {
+        continue;
+      }
+      int choice1 = flip(stone, 0);
+      int choice2 = flip(stone, 1);
+      int choice3 = flip(stone, 2);
+
+      checkStone(worldMap, preferences, priest, choice1, stone);
+      checkStone(worldMap, preferences, priest, choice2, stone);
+      checkStone(worldMap, preferences, priest, choice3, stone);
+    }
   }
 }
 
@@ -55,15 +50,42 @@ int main() {
   for (int round = 0; round < rounds; round++) {
     int numberOfPriests;
     std::cin >> numberOfPriests;
-    std::string currentOrder = "000";
-    for (int priest = 0; priest < numberOfPriests; priest++) {
-      for (int i = 0; i < 8; i++) {
-        int order;
-        std::cin >> order;
-        for (auto selection : orderToBinary(order)) {
-          
-        }
+    worldMapType worldMap;
+    stonesType votes;
+    std::fill(votes.begin(), votes.end(), UNDEFINED);
+    std::fill_n(worldMap.begin(), numberOfPriests + 1, votes);
+
+    worldMapType preferences;
+    for (int i = 1; i <= numberOfPriests; i++) {
+      for (int j = 0; j < 8; j++) {
+        std::cin >> preferences[i][j];
       }
     }
+
+    // Do dynamic programming
+    solve(worldMap, preferences, numberOfPriests);
+
+    std::cout << "world map:\n";
+    for (int row = 0; row <= numberOfPriests; row++) {
+      std::cout << row << ": [ ";
+      for (int col = 0; col < 8; col++) {
+        if (worldMap[row][col] == UNDEFINED) {
+          std::cout << "(" << std::bitset<3>(col) << ": " << UNDEFINED << ") ";
+        } else {
+          std::cout << "(" << std::bitset<3>(col) << ": " << std::bitset<3>(worldMap[row][col]) << ") ";
+        }
+      }
+      std::cout << "]\n";
+    }
+    std::cout << "preferences:\n";
+    for (int row = 1; row <= numberOfPriests; row++) {
+      std::cout << row << ": [ ";
+      for (int col = 0; col < 8; col++) {
+        std::cout << "(" << std::bitset<3>(col) << ": " << preferences[row][col] << ") ";
+      }
+      std::cout << "]\n";
+    }
+
+    // break; // only one round for debugging
   }
 }
